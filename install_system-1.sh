2d906ae1a604ab71af45c2cacbf9bc348cc6a987
Fuis18 <96286214+fuis18@users.noreply.github.com>
2025-12-24 15:18:08 -0500

    Add files via upload

--- install_system-1.sh
+++ install_system-1.sh
@@ -31,10 +31,62 @@ echo -e "${GREEN} ===== Installing Base System ====="
 echo -e "${BLUE} =================================="
 echo -e "${RESET}"
 
-pacman -S --noconfirm base base-devel wayland hyprland hyprlock xdg-desktop-portal xdg-desktop-portal-hyprland
+pacman -S --noconfirm base base-devel wayland hyprland hyprlock
 
-pacman -S --noconfirm gst-libav gst-plugins-good gst-plugins-bad gst-plugins-ugly
+echo ""
+echo -e "${BLUE} =================================="
+echo -e "${GREEN} =========== AUR Helper ==========="
+echo -e "${BLUE} =================================="
+echo -e "${RESET}"
+echo ""
 
+echo ""
+echo -e "${BLUE} ================================="
+echo -e "${GREEN} ========== Aur => paru =========="
+echo -e "${BLUE} ================================="
+echo -e "${RESET}"
+
+PARU_DIR="${USER_REPOS}/paru-bin"
+if [[ -d "$PARU_DIR" ]]; then
+  echo -e "${GREEN}[!] Directorio '$PARU_DIR' ya existe.${RESET}"
+  else
+  git clone https://aur.archlinux.org/paru-bin.git "$PARU_DIR"
+  chown -R "${USER_NAME}:${USER_NAME}" "$PARU_DIR"
+  sudo -u "${USER_NAME}" bash -c "cd '$PARU_DIR' && makepkg -si --noconfirm"
+fi
+
+echo ""
+echo -e "${BLUE} =================================="
+echo -e "${GREEN} =========== Aur => yay ==========="
+echo -e "${BLUE} =================================="
+echo -e "${RESET}"
+
+YAY_DIR="${USER_REPOS}/yay"
+if [[ -d "$YAY_DIR" ]]; then
+  echo -e "${GREEN}[!] Directorio '$YAY_DIR' ya existe.${RESET}"
+  else
+  git clone https://aur.archlinux.org/yay.git "$YAY_DIR"
+  chown -R "${USER_NAME}:${USER_NAME}" "$YAY_DIR"
+  sudo -u "${USER_NAME}" bash -c "cd '$YAY_DIR' && makepkg -si --noconfirm"
+fi
+
+echo ""
+echo -e "${BLUE} ==============================="
+echo -e "${GREEN} ====== System Defatuls ======"
+echo -e "${BLUE} ==============================="
+echo -e "${RESET}"
+
+pacman -S --noconfirm xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+pacman -S --noconfirm papirus-icon-theme
+
+sudo -u fuis18 bash -c 'yay -S papirus-folders-catppuccin-git catppuccin-gtk-theme-mocha'
+
+papirus-folders -C catppuccin-mocha --theme Papirus-Dark
+
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+
+sudo -u fuis18 bash -c 'paru -S wlogout yofi-bin'
+sudo -u fuis18 bash -c 'paru -S ironbar-bin scrub bluetui'
 
 echo ""
 echo -e "${BLUE} ================================="
@@ -52,7 +104,7 @@ echo -e "${GREEN} === Instalando el documentador ==="
 echo -e "${BLUE} =================================="
 echo -e "${RESET}"
 
-pacman -S --noconfirm locate man-db man-pages-es
+pacman -S --noconfirm locate man-db
 
 echo ""
 echo -e "${BLUE} ================================="
@@ -101,8 +153,11 @@ echo -e "${GREEN} ==== Multimedia applications ===="
 echo -e "${BLUE} ================================="
 echo -e "${RESET}"
 
+pacman -S --noconfirm gst-libav gst-plugins-good gst-plugins-bad gst-plugins-ugly
 pacman -S --noconfirm pipewire pipewire-pulse wireplumber
 pacman -S --noconfirm pipewire-alsa alsa-utils
+
+pacman -S --noconfirm pulsemixer
 
 pacman -S --noconfirm cava mpd mpc mpv ncmpcpp
 
--- install_system-2.sh
+++ install_system-2.sh
@@ -26,57 +26,92 @@ echo -e "${RESET}"
 pacman -Syu --noconfirm
 
 echo ""
-echo -e "${BLUE} =================================="
-echo -e "${GREEN} =========== AUR Helper ==========="
-echo -e "${BLUE} =================================="
-echo -e "${RESET}"
-echo ""
-
-echo ""
-echo -e "${BLUE} ================================="
-echo -e "${GREEN} ========== Aur => paru =========="
-echo -e "${BLUE} ================================="
+echo -e "${BLUE} ================================"
+echo -e "${GREEN} === Configurando el terminal ==="
+echo -e "${BLUE} ================================"
 echo -e "${RESET}"
 
-PARU_DIR="${USER_REPOS}/paru-bin"
-if [[ -d "$PARU_DIR" ]]; then
-  echo -e "${GREEN}[!] Directorio '$PARU_DIR' ya existe.${RESET}"
-  else
-  git clone https://aur.archlinux.org/paru-bin.git "$PARU_DIR"
-  chown -R "${USER_NAME}:${USER_NAME}" "$PARU_DIR"
-  sudo -u "${USER_NAME}" bash -c "cd '$PARU_DIR' && makepkg -si --noconfirm"
+PLUGIN_DIR="/usr/share/zsh-sudo"
+mkdir -p "${PLUGIN_DIR}"
+chown -R "${USER_NAME}:${USER_NAME}" "${PLUGIN_DIR}"
+sudo -u "${USER_NAME}" wget -qO "${PLUGIN_DIR}/sudo.plugin.zsh" \
+  https://raw.githubusercontent.com/hcgraf/zsh-sudo/master/sudo.plugin.zsh
+
+echo ""
+echo -e "${BLUE} =================================="
+echo -e "${GREEN} ==== Instalando Login Manager ===="
+echo -e "${BLUE} =================================="
+echo -e "${RESET}"
+
+pacman -S --noconfirm greetd greetd-tuigreet
+
+if id greeter &>/dev/null; then
+    echo "✔ El usuario greeter ya existe. Continuando..."
+else
+    useradd -r -s /usr/bin/nologin -d /var/lib/greetd -M greeter
 fi
 
-echo ""
-echo -e "${BLUE} =================================="
-echo -e "${GREEN} =========== Aur => yay ==========="
-echo -e "${BLUE} =================================="
-echo -e "${RESET}"
+cp -r "${FUIS_REPO}/etc/greetd/." /etc/greetd/
 
-YAY_DIR="${USER_REPOS}/yay"
-if [[ -d "$YAY_DIR" ]]; then
-  echo -e "${GREEN}[!] Directorio '$YAY_DIR' ya existe.${RESET}"
-  else
-  git clone https://aur.archlinux.org/yay.git "$YAY_DIR"
-  chown -R "${USER_NAME}:${USER_NAME}" "$YAY_DIR"
-  sudo -u "${USER_NAME}" bash -c "cd '$YAY_DIR' && makepkg -si --noconfirm"
-fi
+chmod 644 /etc/greetd/config.toml
+systemctl enable greetd
 
 echo ""
 echo -e "${BLUE} ================================="
-echo -e "${GREEN} ====== paru's Dependencies ======"
+echo -e "${GREEN} ====== Copiying Root Files ======"
 echo -e "${BLUE} ================================="
 echo -e "${RESET}"
 
-sudo -u fuis18 bash -c 'paru -S wlogout yofi-bin ironbar-bin scrub bluetui'
+cp -r "${FUIS_REPO}/root/config/." /root/.config/
+
+cp -r "${FUIS_REPO}/root/zshrc" /root/
+# Renombrar
+mv /root/zshrc /root/.zshrc
+
+echo -e "${BLUE} ================================="
+echo -e "${GREEN} ============= Fonts ============="
+echo -e "${BLUE} ================================="
+echo -e "${RESET}"
+
+chown -R "${USER_NAME}:${USER_NAME}" "${USER_HOME}/Downloads"
+
+wget -O "${USER_HOME}/Downloads/FiraCode.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
+
+unzip "${USER_HOME}/Downloads/FiraCode.zip" -d /usr/share/fonts
+
+pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu ttf-liberation
+
+fc-cache -fv
+
+echo ""
+echo -e "${BLUE} ================================="
+echo -e "${GREEN} ===== Actualizando el Shell ====="
+echo -e "${BLUE} ================================="
+echo -e "${RESET}"
+
+ZSH_PATH="$(command -v zsh || true)"
+
+echo "-> Cambiando shell a zsh..."
+usermod --shell "$ZSH_PATH" root
+usermod --shell "$ZSH_PATH" "$USER_NAME"
+
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+chmod 440 /etc/sudoers.d/wheel
 
 echo ""
 echo -e "${BLUE} =================================="
-echo -e "${GREEN} ======= yay's Dependencies ======="
+echo -e "${GREEN} ==== Creating the directories ===="
 echo -e "${BLUE} =================================="
 echo -e "${RESET}"
 
-sudo -u fuis18 bash -c 'yay -S swayimg'
+mkdir -p "${USER_HOME}/Documents"
+chown "${USER_NAME}:${USER_NAME}" "${USER_HOME}/Documents"
+mkdir -p "${USER_HOME}/Music"
+chown "${USER_NAME}:${USER_NAME}" "${USER_HOME}/Music"
+mkdir -p "${USER_HOME}/Images"
+chown "${USER_NAME}:${USER_NAME}" "${USER_HOME}/Images"
+mkdir -p "${USER_HOME}/Scripts"
+chown "${USER_NAME}:${USER_NAME}" "${USER_HOME}/Scripts"
 
 echo ""
 echo -e "${BLUE}=================================="
