8d91db847b121c5cc6ac5285d0e153ecea9267fe
Fuis18 <96286214+fuis18@users.noreply.github.com>
2026-01-02 21:07:12 -0500

    Add files via upload

--- install_personal-1.sh
+++ install_personal-1.sh
@@ -21,7 +21,7 @@ echo -e "${GREEN} ====== Updating the System ======="
 echo -e "${BLUE} =================================="
 echo -e "${RESET}"
 
-pacman -Syu --noconfirm
+pacman -Syu
 
 echo ""
 echo -e "${BLUE} ================================="
@@ -35,13 +35,7 @@ pacman -S --noconfirm pipewire-alsa alsa-utils
 pacman -S --noconfirm mpd mpc mpv ncmpcpp fftw lsp-plugins-lv2
 pacman -S --noconfirm pulsemixer cava easyeffects rubberband
 
-echo ""
-echo -e "${BLUE} ================================="
-echo -e "${GREEN} ====== Captura de Pantalla ======"
-echo -e "${BLUE} ================================="
-echo -e "${RESET}"
-
-pacman -S --noconfirm grim slurp swappy
+sudo -u "$USER_NAME" mkdir -p "$USER_HOME/.config/mpd/playlists"
 
 echo ""
 echo -e "${BLUE} ================================="
@@ -79,6 +73,15 @@ sudo -u fuis18 bash -c 'yay -S librewolf-bin brave-bin'
 
 sudo pacman -S --noconfirm syncthing
 sudo pacman -S --noconfirm rclone
+
+echo ""
+echo -e "${BLUE} =================================="
+echo -e "${GREEN} ============ Keyboard ==========="
+echo -e "${BLUE} =================================="
+echo -e "${RESET}"
+
+sudo pacman -S --noconfirm fcitx5 fcitx5-mozc fcitx5-configtool
+sudo pacman -S --noconfirm fcitx5-gtk fcitx5-qt
 
 echo ""
 echo -e "${BLUE} =================================="
